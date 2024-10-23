ff6165fa476ef2f42b108b75d533e347ec74df6c
nhaar <38634785+nhaar@users.noreply.github.com>
2024-10-23 17:01:29 -0300

    add server information endpoint

---
+++ src/app/api/server/route.ts
@@ -0,0 +1,30 @@
+import { CURRENT_VERSION } from '@/lib/version'
+import { createJsonPost, OSInformation } from "@/lib/api"
+import { ErrorResponse } from "@/lib/res"
+
+export const dynamic = 'force-dynamic'
+
+export const POST = createJsonPost(async (body) => {
+  const parse = OSInformation.safeParse(body)
+  if (parse.success) {
+    const { platform, arch } = parse.data
+
+    const version = CURRENT_VERSION.version;
+
+    if (platform === 'win32') {
+      if (arch === 'x64') {
+        return Response.json({
+          'exists': true,
+          'filename': `server-${version}-win.zip`,
+          'name': 'server',
+          'type': 'server'
+        })
+      }
+    }
+    return Response.json({
+      'exists': false
+    })
+  } else {
+    return ErrorResponse.create(400, 'Incorrect body');
+  }
+})
