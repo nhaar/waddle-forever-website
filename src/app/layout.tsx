d80f1cdb449b8876d9afdd1a46d070299118d57f
Randomno <emailrandomno@gmail.com>
2025-08-16 02:33:16 +0100

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: 'Waddle Forever',
@@ -14,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="min-h-screen flex flex-col bg-[#325597] text-white">
         <Navbar />
-        <main className="bg-[#325597] text-white flex justify-center h-screen">{children}</main>
+        <main className="flex-1 flex justify-center">{children}</main>
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer(){
+    return(
+        <footer>
+            <hr/>
+            <p className="text-sm text-gray-300">Not affiliated with Disney. Waddle Forever is free and not-for-profit.</p>
+        </footer>
+    )
+}
